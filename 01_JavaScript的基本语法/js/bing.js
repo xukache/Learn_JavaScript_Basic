e9878dfb92fe7a8d422f62c9cfce0229282e0d77
kachex <2416695482@qq.com>
2022-12-01 23:04:56 +0800

    first commit

---
+++ 01_JavaScript的基本语法/js/bing.js
@@ -0,0 +1,6 @@
+// 获取元素
+var bingAEl = document.querySelector(".bing")
+
+bingAEl.onclick = function() {
+  alert("bing一下")
+}
---
+++ 01_JavaScript的基本语法/js/文档注释.js
@@ -0,0 +1,10 @@
+/**
+ * 和某人打招呼的函数
+ * @param {string} name 姓名
+ * @param {number} age 年龄
+ */
+ function sayHello(name, age) {
+
+ }
+
+ sayHello('kachex', 18)
